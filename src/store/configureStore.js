cf10f967dfef425ec1f5175395719c15217cce6d
Lluis Suros <lluissuros@gmail.com>
2017-08-28 17:26:14 +0400

    Add Redux DevTools extesion

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,9 +4,13 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
+    composeEnhancers(
+      applyMiddleware(thunk, reduxImmutableStateInvariant())
+    )
   );
 }
